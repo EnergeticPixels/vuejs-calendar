11d4fbf57f2d8c670e036ade06033c92a333f510
Anthony Jackman <energeticpixels@gmail.com>
2018-04-01 18:28:33 -0500

    adding buttons to current month comp

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -1,11 +1,21 @@
 <template>
     <div>
         <div>{{ formattedDate }}</div>
+        <button @click='dec'>-</button>
+        <button @click='inc'>+</button>
     </div>
 </template>
 
 <script>
     export default {
+        methods: {
+            inc() {
+                console.log('inked');
+            },
+            dec() {
+
+            } 
+        },
         computed: {
             month() {
                 return this.$store.state.currentMonth;
