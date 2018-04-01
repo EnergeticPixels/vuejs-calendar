bcd0114cdf89ef4c17d512fbcaf94f52b3b34445
Anthony Jackman <energeticpixels@gmail.com>
2018-04-01 18:24:11 -0500

    display date in current month comp

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
-
+        <div>{{ formattedDate }}</div>
     </div>
 </template>
 
 <script>
     export default {
-        
+        computed: {
+            month() {
+                return this.$store.state.currentMonth;
+            },
+            year() {
+                return this.$store.state.currentYear;
+            },
+            formattedDate() {
+                return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format('MMMM YYYY');
+            }
+        }
     }
 </script>
 
