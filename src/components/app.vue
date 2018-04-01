e2130f54af14849663da4633e9f552aafa300f64
Anthony Jackman <energeticpixels@gmail.com>
2018-03-31 19:57:42 -0500

    padding days to start/end of month to display full weeks

--- src/components/app.vue
+++ src/components/app.vue
@@ -8,18 +8,44 @@
     export default {
         data() {
             return {
-                month: 2,
+                month: 5,
                 year: 2017
             };
         },
         computed: {
             days() {
+                
+                // generatign all days in current month
                 let days = [];
                 let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D');
                 do {
                     days.push(currentDay);
                     currentDay = this.$moment(currentDay).add(1, 'days');
                 } while ((currentDay.month()+1) === this.month);
+
+                // add previous days to start
+                currentDay = this.$moment(days[0]);
+                const SUNDAY = 0;
+                const MONDAY = 1;
+                if (currentDay.day() !== MONDAY) {
+                        do {
+                        currentDay = this.$moment(currentDay).subtract(1, 'days');
+                        // unshift pushes to the start of the array instead of the tail
+                        days.unshift(currentDay);
+                    } while(currentDay.day() !== MONDAY);
+                }
+                
+
+                // add end days til end
+                currentDay = this.$moment(days[days.length - 1]);
+                if (currentDay.day() !== SUNDAY) {
+                    do {
+                        currentDay = this.$moment(currentDay).add(1, 'days');
+                        // unshift pushes to the start of the array instead of the tail
+                        days.push(currentDay);
+                    } while(currentDay.day() !== SUNDAY);
+                }
+                
                 return days;
             }
         }
