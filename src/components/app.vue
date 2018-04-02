94dff40c71705d263a7cc9b115787ac60da2d366
Anthony Jackman <energeticpixels@gmail.com>
2018-04-01 19:46:48 -0500

    create event-form comp

--- src/components/app.vue
+++ src/components/app.vue
@@ -22,12 +22,14 @@
                 <calendar-day v-for='day in week' v-bind:day='day'></calendar-day>
             </div>
         </div>
+        <event-form></event-form>
     </div>
 </template>
 
 <script>
     import CalendarDay from './CalendarDay.vue';
     import CurrentMonth from './CurrentMonth.vue';
+    import EventForm from './EventForm.vue';
     export default {
         computed: {
             month() {
@@ -87,7 +89,8 @@
         },
         components: {
             CalendarDay,
-            CurrentMonth
+            CurrentMonth,
+            EventForm
         }
     }
 </script>
