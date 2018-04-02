e22766406ee76f3f616c99ed0071e73e5fd23376
Anthony Jackman <energeticpixels@gmail.com>
2018-04-01 21:15:22 -0500

    form placeholder and mandatory user input for submit

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -2,7 +2,7 @@
     <div id='event-form' :class='{ active: active }' :style="{ top: top, left: left }">
         <h4>Add an event</h4>
         <div class='text'>
-            <input type='text' v-model='description' />
+            <input type='text' v-model='description' placeholder='dinner at panchos'/>
         </div>
         <div class='buttons'>
             <button @click='create'>Create</button>
@@ -23,7 +23,12 @@
                 this.$store.commit('eventFormActive', false);
             },
             create() {
-                this.$store.commit('addEvent', this.description);
+                if (this.description.length === 0) {
+                    this.$store.commit('addEvent', this.description);
+                    this.description = '';
+                    this.$store.commit('eventFormActive', false);
+                }
+            
             }
         },
         computed: {
